4b3a8090b163b7bb7dda86f8d2a43cad14665652
saferboy <muxtordev@gmail.com>
2023-05-19 17:43:32 +0500

    project start

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import cors from 'cors'
+
+// import api from "./routes/router"
+
+const app = express()
+const port = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// app.use('/api', api)
+
+app.listen(port, () => {
+    console.log(`Server is running is ${port}`);
+})
---
+++ src/service/index.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+import {addAliases} from "module-alias"
+
+addAliases({
+    '@controller': `${__dirname}/controller`,
+    '@middleware': `${__dirname}/middleware`,
+    '@model':      `${__dirname}/model`,
+    '@service':    `${__dirname}/service`,
+    '@config' :    `${__dirname}/config`,
+    '@utils' :     `${__dirname}/utils`
+})
+
+dotenv.config()
